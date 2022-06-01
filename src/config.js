5b977f159a2bd464f50910e8b045c812019ab7d5
apollo0102 <crazyprince0012@gmail.com>
2022-06-01 10:53:54 -0400

    fff

--- src/config.js
+++ src/config.js
@@ -1,17 +1,26 @@
 const layersOrder = [
-    { name: 'background', number: 1 },
-    { name: 'ball', number: 2 },
-    { name: 'eye color', number: 12 },
-    { name: 'iris', number: 3 },
-    { name: 'shine', number: 1 },
-    { name: 'shine', number: 1 },
-    { name: 'bottom lid', number: 3 },
-    { name: 'top lid', number: 3 },
+    // { name: 'background', number: 1 },
+    // { name: 'ball', number: 2 },
+    // { name: 'eye color', number: 12 },
+    // { name: 'iris', number: 3 },
+    // { name: 'shine', number: 1 },
+    // { name: 'shine', number: 1 },
+    // { name: 'bottom lid', number: 3 },
+    // { name: 'top lid', number: 3 },
+    { name: 'Background', number: 7 },
+    { name: 'Base', number: 1 },
+    { name: 'Body', number: 11 },
+    { name: 'Clothes', number: 24 },
+    { name: 'Eyes', number: 20 },
+    { name: 'Fly', number: 2 },
+    { name: 'Head', number: 22 },
+    { name: 'Masks', number: 5 },
+    { name: 'Mouth', number: 9 },
 ];
   
 const format = {
-    width: 230,
-    height: 230
+    width: 3000,
+    height: 3000
 };
 
 const rarity = [
